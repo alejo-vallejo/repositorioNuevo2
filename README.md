2a560378c658c4519796a31b74fb26fe2e24b901
alejo-vallejo <alejandro.garcia6@utp.edu.co>
2026-02-25 00:40:12 +0000

    primer commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# repositorioNuevo2
---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from dotenv import load_detenv
+
+app = FastAPI()
+
+load_dotenv()
+
+@app.get("/llm/{prompt}")
+async def read_root(prompt):
+
+    #Crear una logica que me permita comunicarme con un elemento
+    from google import genai
+
+    # The client gets the API key from the environment variable `GEMINI_API_KEY`.
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents= prompt
+    )
+    print(response.text)
+
+    return {"Respuesta": response.text}
